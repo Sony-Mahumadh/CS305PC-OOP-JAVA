31ffa4dc40f2896991621ffb75befe510ce6f403
Sony-Mahumadh <sm1269049@gmail.com>
2025-10-18 19:31:06 +0530

    Update Lab2P4.java

--- LAB2/Lab2P4.java
+++ LAB2/Lab2P4.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class Lab2P45 {
+class Lab2P4 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.println("Enter a value");
@@ -10,4 +10,5 @@ class Lab2P45 {
         System.out.println("Bitwise AND: " + (a & b));
         s.close();
     }
+
 }
